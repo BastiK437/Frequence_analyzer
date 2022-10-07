2424deb647c1a5209d1cebd8b44bcd99eb7bac7d
Bastian Krammer <bastiankrammer@mailbox.org>
2022-10-07 22:13:36 +0200

    wip

---
+++ include/main.h
@@ -0,0 +1,41 @@
+#include <soundio/soundio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <errno.h>
+#include <unistd.h>
+
+struct RecordContext {
+    struct SoundIoRingBuffer *ring_buffer;
+};
+
+static enum SoundIoFormat prioritized_formats[] = {
+    SoundIoFormatFloat32NE,
+    SoundIoFormatFloat32FE,
+    SoundIoFormatS32NE,
+    SoundIoFormatS32FE,
+    SoundIoFormatS24NE,
+    SoundIoFormatS24FE,
+    SoundIoFormatS16NE,
+    SoundIoFormatS16FE,
+    SoundIoFormatFloat64NE,
+    SoundIoFormatFloat64FE,
+    SoundIoFormatU32NE,
+    SoundIoFormatU32FE,
+    SoundIoFormatU24NE,
+    SoundIoFormatU24FE,
+    SoundIoFormatU16NE,
+    SoundIoFormatU16FE,
+    SoundIoFormatS8,
+    SoundIoFormatU8,
+    SoundIoFormatInvalid,
+};
+
+static int prioritized_sample_rates[] = {
+    48000,
+    44100,
+    96000,
+    24000,
+    0,
+};
